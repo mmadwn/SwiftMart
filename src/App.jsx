56e5c7f74bb5b537b0d3511de6bf5974e57535e5
Ridwan <49033429+mmadwn@users.noreply.github.com>
2024-10-10 18:35:42 +0700

    feat: rm slick

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 
 export default function App() {
   return <RouterProvider router={router} />;
